4e13fe5a3e30ab003d16596ff23b89ae53fe3e26
Bratah123 <58405975+Bratah123@users.noreply.github.com>
2020-01-29 18:06:22 -0800

    Added Player Movement

---
+++ SnowHead/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Rigidbody2D player;
+
+    public float moveSpeed = 4f;
+    public float bulletSpeed = 10f;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            Shoot();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        player.MovePosition(player.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+    
+    void Shoot()
+    {
+        Debug.Log("Shoot!");
+    }
+}

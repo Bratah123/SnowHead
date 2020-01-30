2b1722a80fbd4fe062f8e103275621e9ae745998
Bratah123 <58405975+Bratah123@users.noreply.github.com>
2020-01-29 18:44:22 -0800

    Added an HP Bar

--- SnowHead/Assets/Scripts/Player.cs
+++ SnowHead/Assets/Scripts/Player.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+
+    public static int health = 100;
+
+    public Image[] HpBar;
 
     public Rigidbody2D player;
 
@@ -22,6 +27,8 @@ public class Player : MonoBehaviour
         {
             Shoot();
         }
+
+        HealthBarCalc();
     }
 
     void FixedUpdate()
@@ -33,4 +40,54 @@ public class Player : MonoBehaviour
     {
         Debug.Log("Shoot!");
     }
-}
+
+    void HealthBarCalc()
+    {
+        if(Player.health <= 90)
+        {
+            HpBar[9].GetComponent<Image>().enabled = false;
+        }
+        else if(Player.health <= 80)
+        {
+            HpBar[8].GetComponent<Image>().enabled = false;
+        }
+        else if (Player.health <= 70)
+        {
+            HpBar[7].GetComponent<Image>().enabled = false;
+        }
+        else if (Player.health <= 60)
+        {
+            HpBar[6].GetComponent<Image>().enabled = false;
+        }
+        else if (Player.health <= 50)
+        {
+            HpBar[5].GetComponent<Image>().enabled = false;
+        }
+        else if (Player.health <= 40)
+        {
+            HpBar[4].GetComponent<Image>().enabled = false;
+        }
+        else if (Player.health <= 30)
+        {
+            HpBar[3].GetComponent<Image>().enabled = false;
+        }
+        else if (Player.health <= 20)
+        {
+            HpBar[2].GetComponent<Image>().enabled = false;
+        }
+        else if (Player.health <= 10)
+        {
+            HpBar[1].GetComponent<Image>().enabled = false;
+        }
+        else if (Player.health <= 0)
+        {
+            HpBar[0].GetComponent<Image>().enabled = false;
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Dead");
+    }
+ }
